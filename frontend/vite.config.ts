4e4454109f6a9e1fe93f2e142ca977dff88fc671
SuperPuper545 <91686638+SuperPuper545@users.noreply.github.com>
2026-09-03 10:13:57 +0500

    feat: enable allowedHosts for tunnels and set web_app button support

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
@@ -11,6 +12,7 @@ export default defineConfig({
   server: {
     host: true,
     port: 5173,
+    allowedHosts: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
